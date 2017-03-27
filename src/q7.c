eede470fdd29162b39fc6bc536788718e34a2a31
Adithya <adithyakreddy6@gmail.com>
2017-03-27 07:11:43 +0530

    q7 refactor and explanation

--- src/q7.c
+++ src/q7.c
@@ -3,8 +3,44 @@
  * to execute the ps command to verify that the process is a zombie
  */
 
+/*
+ * fork - create a child process
+ * ==============================
+ *
+ * #include <unistd.h>
+ *
+ * pid_t fork(void);
+ *
+ * Creates a new process by duplicating the calling process. The child
+ * process is run in a separate memory space but duplictes the parent's
+ * memory space.
+ *
+ * Returns PID of the child process in the parent, 0 in the child, or
+ * -1 on failure to create and errno is set appropriately.
+ */
+
+/*
+ * system - execute a shell command
+ * =================================
+ *
+ * #include <stdlib.h>
+ *
+ * int system(const char* command);
+ *
+ * Uses fork() to create a child process that executes the shell
+ * command specified. The shell command's output will be streamed
+ * to STDOUT. system() returns after the command has been completed
+ */
+
+/*
+ * ps - report a snapshot of current processes
+ * ============================================
+ *
+ * -A -- Show all processes (by default only processes associated
+ *       with the current terminal are shown).
+ */
+
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -16,6 +52,6 @@ int main(void) {
     else
         sleep(3);
 
-    system("ps -ax");
+    system("ps -A | grep q7");
     return 0;
 }
