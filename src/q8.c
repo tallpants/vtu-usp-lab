ed92f70fe75674da76c059f7253baf8146df005d
adithya <adithyakreddy6@gmail.com>
2017-05-08 10:57:39 +0530

    q8

--- src/q8.c
+++ src/q8.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(void) {
-  printf("Hello");
+int main(void)
+{
+  int pid1, pid2;
+  pid1 = fork();
+
+  if (pid1 < 0)
+  {
+    printf("Fork fails");
+    exit(0);
+  }
+
+  else if (pid1 == 0)
+  {
+    pid2 = fork();
+    if (pid2 < 0)
+    {
+      printf("fork fails");
+      exit(0);
+    }
+
+    else if (pid2 == 0)
+    {
+      sleep(1);
+      printf("The PID of parent process is %d", getppid());
+      exit(0);
+    }
+
+    else if (pid2 > 0)
+      exit(0);
+  }
+
+  else if (pid1 > 0)
+    wait(0);
+  
+  system("ps -o pid,ppid,stat,cmd");
+
   return 0;
 }
